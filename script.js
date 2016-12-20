3104072fcb89e73679131d39281305d0a5261ece
Tommy <TommyCohn116@gmail.com>
2016-12-20 14:21:10 -0500

    Create text fields for inputting the matrix, and create a javascript array to hold the html objects.

--- script.js
+++ script.js
@@ -3,22 +3,51 @@ var cols;
 var rowInput;
 var colInput;
 var calcButton;
+var inputArea;
 var givenMatrix;
+var htmlMatrix;
 
 function setup() {
 	rowInput = document.getElementById("rows");
 	colInput = document.getElementById("cols");
 	calcButton = document.getElementById("calc");
+	inputArea = document.getElementById("inputArea");
 
 	rowInput.addEventListener("input", updateInputDimensions);
 	colInput.addEventListener("input", updateInputDimensions);
 	calcButton.addEventListener("click", calculate);
 }
 function updateInputDimensions() {
+	var r = Number(rowInput.value);
+	var c = Number(colInput.value);
+	if(isNaN(r) || isNaN(c)) {
+		return;
+	}
+	else {
+		htmlMatrix = [];
+		rows = r;
+		cols = c;
 
+		inputArea.innerHTML = "";
+
+		for(var i=0; i<rows; ++i) {
+			htmlMatrix.push([]);
+			for(var j=0; j<cols; ++j) {
+				inputArea.innerHTML += "<textarea id='matrix_"+i+"_"+j+"' style='width:40px; height:40px; resize:none;'></textarea>";
+			}
+			inputArea.innerHTML += "<br>"
+		}
+		window.setTimeout(function() {
+			for(var i=0; i<rows; ++i) {
+				for(var j=0; j<cols; ++j) {
+					htmlMatrix[i].push(document.getElementById("matrix_"+i+"_"+j));
+				}
+			}
+		}, 0);
+	}
 }
 function calculate() {
-	
+
 }
 
 setup();
